5b8612d5b242171bc85dd2296b0906035ed5818e
victordiaz-afk <victor.diaz@csuglobal.edu>
2025-10-07 09:31:00 -0400

    Update critial_thinking_assignment_1.py
    Changed, num_3 and num_4 to num_1 and num_2, to better fit assignment criteria

--- critial_thinking_assignment_1.py
+++ critial_thinking_assignment_1.py
@@ -9,15 +9,16 @@ print("The subtraction of", num_1, "and", num_2, "is", subtraction, '\n')
 
 
 print("This program multiplies and divides two numbers.")
-num_3 = input("Enter first number: ")
-num_4 = input("Enter second number: ")
+num_1 = input("Enter first number: ")
+num_2 = input("Enter second number: ")
 
-multiplication = float(num_3) * float(num_4)
-print("The multiplication of", num_3, "and", num_4, "is", multiplication)
-if float(num_4) != 0:
-    division = float(num_3) / float(num_4)
-    print("The division of", num_3, "and", num_4, "is", division)
+multiplication = float(num_1) * float(num_2)
+print("The multiplication of", num_1, "and", num_2, "is", multiplication)
+if float(num_2) != 0:
+    division = float(num_1) / float(num_2)
+    print("The division of", num_1, "and", num_2, "is", division)
 else:
     print("Division by zero is not allowed.")
+
 
  
